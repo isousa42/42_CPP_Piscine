d1cf4d10661bfe2da6c3e80e1cf26ca408e8ef03
Inês A. Sousa <isousa@student.42lisboa.com>
2021-10-15 07:12:43 +0100

    started module 2 ex00

---
+++ 2_Module/Fixed.cpp
@@ -0,0 +1,18 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    _fixed_point = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Dstructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+    _fixed_point = fixed._fixed_point;
+}
+
---
+++ 2_Module/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+
+    private:
+
+        int _fixed_point;
+        static const int _frac_bits = 8;
+
+    public:
+
+        Fixed();
+        ~Fixed();
+        
+
+
+};
+
+
+
+
+#endif
