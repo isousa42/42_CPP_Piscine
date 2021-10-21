7236425f1f85a26b65bdd68d92c9a6d08fe40d86
Inês A. Sousa <isousa@student.42lisboa.com>
2021-10-21 13:56:54 +0100

    finish ex04 module 1

--- 1_Module/ex04/main.cpp
+++ 1_Module/ex04/main.cpp
@@ -7,13 +7,66 @@ int main(int argc, char **argv)
     std::string s1;
     std::string s2;
     std::fstream file;
+    std::fstream new_file;
+    std::string str;
+    std::string name_file;
+    char c;
 
-    if ()
+    if (argc != 4)
+    {
+        std::cout << "ERROR ARGUMENTS" << std::endl;
+        return (1);
+    }
 
+    name_file = argv[1];
+    name_file.append(".replace");
     s1 = argv[2];
     s2 = argv[3];
-    file.open(argv[1], std::ios::out | std::ios::trunc);
 
+    file.open(argv[1], std::ios::in);
+    if (!file)
+    {
+        std::cout << "ERROR OPENING FILE" << std::endl;
+        return (1);
+    }
+    new_file.open(name_file, std::ios::out | std::ios::in | std::ios::trunc);
+    {
+        std::cout << "ERROR CREATING FILE" << std::endl;
+        return (1);
+    }
+    while (file.get(c))
+    {
+        std::cout << c;
+        if (c == ' ' || c == '\n')
+        {
+            if (str.compare(s1) == 0)
+            {
 
+                new_file << s2;
+                new_file << c;
+                str.clear();
+            }
+            else
+            {
+                new_file << str;
+                new_file << c;
+                str.clear();
+
+            }
+        }
+        else
+            str.append(1, c);
+    }
+    if (str.compare(s1) == 0)
+    {
+
+        new_file << s2;
+        str.clear();
+    }
+    else
+        new_file << str;
+    new_file.close();
     file.close();
+
+    return (0);
 }
