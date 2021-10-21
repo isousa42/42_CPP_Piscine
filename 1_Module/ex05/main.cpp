b0c74af40a16044b6d6f36d240b5d2f81332e5df
Inês A. Sousa <isousa@student.42lisboa.com>
2021-10-21 16:03:03 +0100

    finish ex05 module 1'

---
+++ 1_Module/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Karen.hpp"
+
+int main(void)
+{
+    Karen karen;
+
+    karen.complain("DEBUG");
+    karen.complain("INFO");
+    karen.complain("WARNING");
+    karen.complain("ERROR");
+    karen.complain("INFO");
+
+
+    return (0);
+}
