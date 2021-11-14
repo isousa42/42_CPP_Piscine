48ec1b669abd14b840a324441d5afcf1c123aca2
Inês A. Sousa <isousa@student.42lisboa.com>
2021-11-14 00:29:59 +0000

    EX04 DONE ON MODULE 1 FINALLY

--- 1_Module/ex04/main.cpp
+++ 1_Module/ex04/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <fstream>
 
-
+void    ft_replace(std::fstream &file, std::fstream &new_file, std::string s1, std::string s2)
+{
+    int pos = 0;
+    int save_pos = 0;
+    std::string new_str;
+    std::string test;
+    while (std::getline(file, test))
+    {
+        while ((pos = test.find(s1, save_pos)) >= 0)
+        {
+            new_str.append(test.substr(save_pos, pos - save_pos));
+            new_str.append(s2);
+            save_pos = pos + s1.length();
+        }
+        new_str.append(test.substr(save_pos, pos - save_pos));
+        if (new_str.empty())
+                new_file << test;
+        else
+            new_file << new_str;
+        save_pos = 0;
+        pos = 0;
+        new_str.erase();
+        if (!file.eof())
+            new_file << std::endl;
+    }
+    return ;
+}
 
 int main(int argc, char **argv)
 {
@@ -9,22 +35,17 @@ int main(int argc, char **argv)
     std::string s2;
     std::fstream file;
     std::fstream new_file;
-    std::string str;
     std::string name_file;
-
-    std::string test;
 
     if (argc != 4)
     {
         std::cout << "ERROR ARGUMENTS" << std::endl;
         return (1);
     }
-
     name_file = argv[1];
     name_file.append(".replace");
     s1 = argv[2];
     s2 = argv[3];
-
     file.open(argv[1], std::ios::in);
     if (!file)
     {
@@ -37,32 +58,7 @@ int main(int argc, char **argv)
         std::cout << "ERROR CREATING FILE" << std::endl;
         return (1);
     }
-    int pos = 0;
-    int save_pos = 0;
-    std::string new_str;
-    while (std::getline(file, test))
-    {
-        std::cout << "test = " << test << std::endl;
-        while ((pos = test.find(s1, save_pos)) >= 0)
-        {
-            if (pos >= 0 && pos < (int)test.length())
-            {
-                new_str.append(test.substr(save_pos, pos - save_pos));
-                new_str.append(s2);
-                save_pos = pos + s1.length();
-                std::cout << "new 1 = " << new_str << std::endl;
-            }
-        }
-        if (new_str.empty())
-                new_file << test;
-        else
-            new_file << new_str;
-        save_pos = 0;
-        pos = 0;
-        new_str.erase();
-        new_file << std::endl;
-    }
-     std::cout << "test = " << test << std::endl;
+    ft_replace(file, new_file, s1, s2);
     new_file.close();
     file.close();
 
