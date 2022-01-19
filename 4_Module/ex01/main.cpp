66557bd338415aba6aa69164d43e74eadc5e55f8
isousa <seni.alexandra98@gmail.com>
2022-01-19 18:35:55 +0000

    finish ex01 module 4

--- 4_Module/ex01/main.cpp
+++ 4_Module/ex01/main.cpp
@@ -7,15 +7,15 @@
 
 int main()
 {
-    // const Animal* array[10];
+    const Animal* array[10];
 
-    // for(int i = 0; i < 5; i++)
-    //     array[i] = new Dog();
-    // for(int i = 5; i < 10; i++)
-    //     array[i] = new Cat();
+    for(int i = 0; i < 5; i++)
+        array[i] = new Dog();
+    for(int i = 5; i < 10; i++)
+        array[i] = new Cat();
 
-    // for(int i = 0; i < 10; i++)
-    //     delete array[i];
+    for(int i = 0; i < 10; i++)
+        delete array[i];
 
     Cat gato;
     Cat copia(gato);
