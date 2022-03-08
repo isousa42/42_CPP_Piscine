4f3b261f7abefa665fafc3aa73c4bb58bf40473f
isousa42 <seni.alexandra98@gmail.com>
2022-03-08 11:58:18 +0000

    start ex02 of module 7

--- 7_Module/ex02/Array.hpp
+++ 7_Module/ex02/Array.hpp
@@ -1,5 +1,3 @@
-
-
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
@@ -17,10 +15,89 @@ class Array {
     private:
 
         anyType *array;
+        int     size;
 
+    public:
 
+        Array<anyType>();
+        Array<anyType>(unsigned int size);
+        Array<anyType>(const Array<anyType> &copy);
+        Array<anyType>& operator=(const Array<anyType> &copy);
+        anyType& operator[](int index);
+        ~Array<anyType>();
 
+        int size(void);
 
 };
+
+template <typename anyType>
+Array<anyType>::Array() 
+{
+    _array = NULL;
+    _size = 0;
+}
+
+template <typename anyType>
+Array<anyType>::Array(unsigned int size) 
+{
+    _array = new anyType[size];
+    _size = size;
+
+    int i = 0;
+    while (i < size)
+    {
+        _array[i] = 0;
+        i++;
+    }
+}
+
+template <typename anyType>
+Array<anyType>::Array(const Array<anyType> &copy) 
+{
+    *this = copy;
+}
+
+template <typename anyType>
+Array<anyType>& Array<anyType>::operator=(const Array<anyType> &copy) 
+{
+    delete[] _array;
+    _size = copy.size;
+    if (_size > 0)
+    {
+        _array = new anyType[_size];
+        int i = 0;
+        while (i < _size)
+        {
+            _array[i] = copy.array[i];
+            i++;
+        }
+    }
+    else
+    __array = NULL;
+
+    return (*this);
+
+}
+
+template <typename anyType>
+anyType& Array<anyType>::operator[](int index)
+{
+    if (i >= index || i < 0)
+        throw(std::exception());
+    else
+        return (_array[i]);
+}
+
+template <typename anyType>
+Array<anyType>::~Array() 
+{
+    delete[] _array;
+}
+
+template <typename anyType>
+int Array<anyType>::size(void)
+{
+    return (_size);
+}
 
 #endif
