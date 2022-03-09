1c5710b97ac16420d2e7be74c6190fc81ff06ec7
isousa42 <seni.alexandra98@gmail.com>
2022-03-09 10:12:02 +0000

    ex00 of module 8 is done

--- 8_Module/ex00/easyfind.hpp
+++ 8_Module/ex00/easyfind.hpp
@@ -2,13 +2,29 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
 
 /*
+**  For this exercise, we are introduce to iterators and the concept of "traversing containers".
+**  Basically, it is a way of iterate with each element of a container. An interator represents a certain position of a container
+**  and it is the way to go specially when we are working with templates (we don't know the data type - fo example, to return a position from a funtion).
+**  Check this out for better understanding: https://www.simplilearn.com/tutorials/cpp-tutorial/iterators-in-cpp
 **
+**  Iteratores are used like normal variables, they need to be declared and inicialized.
+**  The best methods to inicialize are begin() - It returns the first iterator - and end() - returns last element - of the container.
+**  Remember indexes for strings in c ? Same thing.
 */
 
 template <typename T>
-
-
+typename T::iterator easyfind(T &haystack, int needle)
+{
+    typename T::iterator index = std::find(haystack.begin(), haystack.end(), needle);
+    if (index == haystack.end())
+    {
+        if (*index != needle)
+            throw (std::exception());
+    }
+    return (index);
+}
 
 #endif
--- 8_Module/ex00/main.cpp
+++ 8_Module/ex00/main.cpp
@@ -1,6 +1,49 @@
 #include "easyfind.hpp"
 
+#include <vector>
+
 int main(void)
 {
     
+    std::vector<int> vec;
+    for(int i = 0; i < 10; i++)
+        vec.push_back(i);
+    for(int i = 0; i < 10; i++)
+        std::cout << "vector: " << vec[i] << std::endl;
+
+    std::vector<int>::iterator index;
+
+
+    try
+    {
+        index = easyfind(vec, -1);
+        std::cout << "found: " << *index << std::endl;
+    }
+    catch(std::exception &error)
+    {
+        std::cout << "Did not found it: " << error.what() << std::endl;
+    }
+
+    try
+    {
+        index = easyfind(vec, 5);
+        std::cout << "found: " << *index << std::endl;
+    }
+    catch(std::exception &error)
+    {
+        std::cout << "Did not found it: " << error.what() << std::endl;
+    }
+
+    try
+    {
+        index = easyfind(vec, 15);
+        std::cout << "found: " << *index << std::endl;
+    }
+    catch(std::exception &error)
+    {
+        std::cout << "Did not found it: " << error.what() << std::endl;
+    }
+
+
+    return (0);
 }
